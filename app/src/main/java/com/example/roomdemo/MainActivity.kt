22c8dbe5fb32da3fb613d5ea0f01a33eda0e4501
ratminen <ghgpoklonik16@gmail.com>
2025-11-15 12:41:27 +0700

    1. create project

---
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.roomdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.roomdemo.ui.theme.RoomDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RoomDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ScreenSetup(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenSetup(modifier: Modifier = Modifier) {
+    MainScreen(modifier)
+}
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+}
