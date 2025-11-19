82fac145304d2da5ec64f2ab08898131b5719e98
ratminen <ghgpoklonik16@gmail.com>
2025-11-19 12:18:01 +0700

    8. Final edit

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.roomdemo
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -17,13 +21,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomdemo.ui.theme.RoomDemoTheme
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,20 +48,127 @@ class MainActivity : ComponentActivity() {
         setContent {
             RoomDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ScreenSetup(modifier = Modifier.padding(innerPadding))
+                    val owner = LocalViewModelStoreOwner.current
+                    owner?.let {
+                        val viewModel: MainViewModel = viewModel(
+                            it,
+                            "MainViewModel",
+                            MainViewModelFactory(
+                                LocalContext.current.applicationContext
+                                        as Application)
+                        )
+                        ScreenSetup(
+                            modifier = Modifier.padding(innerPadding),
+                            viewModel = viewModel
+                        )
+                    }
                 }
+            }
+        }
+
+    }
+
+    @Composable
+    fun ScreenSetup(modifier: Modifier = Modifier, viewModel: MainViewModel) {
+        val allProducts by viewModel.allProducts.observeAsState(listOf())
+        val searchResults by viewModel.searchResults.observeAsState(listOf())
+        MainScreen(
+            modifier = modifier,
+            allProducts = allProducts,
+            searchResults = searchResults,
+            viewModel = viewModel
+        )
+    }
+    @Composable
+    fun MainScreen(
+        modifier: Modifier = Modifier,
+        allProducts: List<Product>,
+        searchResults: List<Product>,
+        viewModel: MainViewModel
+    ) {
+        var productName by remember { mutableStateOf("") }
+        var productQuantity by remember { mutableStateOf("") }
+        var searching by remember { mutableStateOf(false) }
+        val onProductTextChange = { text : String ->
+            productName = text
+        }
+        val onQuantityTextChange = { text : String ->
+            productQuantity = text
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .fillMaxWidth()
+        ) {
+            CustomTextField(
+                title = "Product Name",
+                textState = productName,
+                onTextChange = onProductTextChange,
+                keyboardType = KeyboardType.Text
+            )
+            CustomTextField(
+                title = "Quantity",
+                textState = productQuantity,
+                onTextChange = onQuantityTextChange,
+                keyboardType = KeyboardType.Number
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            ) {
+                Button(onClick = {
+                    if (productQuantity.isNotEmpty()) {
+                        viewModel.insertProduct(
+                            Product(
+                                productName,
+                                productQuantity.toInt()
+                            )
+                        )
+                        searching = false
+                    }
+                }) {
+                    Text("Add")
+                }
+                Button(onClick = {
+                    searching = true
+                    viewModel.findProduct(productName)
+                }) {
+                    Text("Search")
+                }
+                Button(onClick = {
+                    searching = false
+                    viewModel.deleteProduct(productName)
+                }) {
+                    Text("Delete")
+                }
+                Button(onClick = {
+                    searching = false
+                    productName = ""
+                    productQuantity = ""
+                }) {
+                    Text("Clear")
+                }
+                    LazyColumn(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    ) {
+                        val list = if (searching) searchResults else allProducts
+                        item {
+                            TitleRow(head1 = "ID", head2 = "Product", head3 = "Quantity")
+                        }
+                        items(list) { product ->
+                            ProductRow(id = product.id, name = product.productName,
+                                quantity = product.quantity)
+                        }
+                    }
             }
         }
     }
 }
 
-@Composable
-fun ScreenSetup(modifier: Modifier = Modifier) {
-    MainScreen(modifier)
-}
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-}
 @Composable
 fun TitleRow(head1: String, head2: String, head3: String) {
     Row (
@@ -96,4 +219,11 @@ fun CustomTextField(
         textStyle = TextStyle(fontWeight = FontWeight.Bold,
             fontSize = 30.sp)
     )
+}
+
+class MainViewModelFactory(val application: Application) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(application) as T
+    }
 }
